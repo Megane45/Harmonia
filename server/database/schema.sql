ce5d53f9183b8683b188d8284279484dc5c40d85
Megane <megane.loisele@gmail.com>
2024-06-25 16:52:27 +0200

    schemaMySQL

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,17 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+create table category (
+  id int unsigned primary key auto_increment not null,
+  name VARCHAR(255) not null
+)
+crate table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis TEXT not NULL,
+  poster VARCHAR(255) not null,
+  country VARCHAR(255) not null,
+  year int NOT NULL,
+  catergory_id int UNSIGNED not NULL
+  Foreign Key (category_id) REFERENCES catergory(id)
+)
